3cfd1c4e72b9b1ed8bd04fc57300eb2847f898cf
Hazem Mourad <67227765+HazemMourad@users.noreply.github.com>
2022-10-26 21:27:04 +0200

    Add files via upload

---
+++ SIG_BillingSystem/src/com/sales/view/LineDialog.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sales.view;
+
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author DELL
+ */
+public class LineDialog extends JDialog{
+    private final JTextField itemNameField;
+    private final JTextField itemCountField;
+    private final JTextField itemPriceField;
+    private final JLabel itemNameLbl;
+    private final JLabel itemCountLbl;
+    private final JLabel itemPriceLbl;
+    private final JButton okBtn;
+    private final JButton cancelBtn;
+    
+    public LineDialog(SIG_Billing_GUI frame) {
+        itemNameField = new JTextField(20);
+        itemNameLbl = new JLabel("Item Name");
+        
+        itemCountField = new JTextField(20);
+        itemCountLbl = new JLabel("Item Count");
+        
+        itemPriceField = new JTextField(20);
+        itemPriceLbl = new JLabel("Item Price");
+        
+        okBtn = new JButton("OK");
+        cancelBtn = new JButton("Cancel");
+        
+        okBtn.setActionCommand("createLineOK");
+        cancelBtn.setActionCommand("createLineCancel");
+        
+        okBtn.addActionListener(frame.getController());
+        cancelBtn.addActionListener(frame.getController());
+        setLayout(new GridLayout(4, 2));
+        
+        add(itemNameLbl);
+        add(itemNameField);
+        add(itemCountLbl);
+        add(itemCountField);
+        add(itemPriceLbl);
+        add(itemPriceField);
+        add(okBtn);
+        add(cancelBtn);
+        
+        pack();
+    }
+
+    public JTextField getItemNameField() {
+        return itemNameField;
+    }
+
+    public JTextField getItemCountField() {
+        return itemCountField;
+    }
+
+    public JTextField getItemPriceField() {
+        return itemPriceField;
+    }
+}
